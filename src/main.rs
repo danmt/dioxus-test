3281cba65867340dee55a92e4e4815443da36829
Dan <danielarturomt@gmail.com>
2024-10-01 18:33:54 +0100

    feat: connect form values with a server function

--- src/main.rs
+++ src/main.rs
@@ -43,8 +43,21 @@ fn Home() -> Element {
                     "Avoid scams, use the cryptography for what it's meant to be."
                 }
             }
-            div {
+            form {
                 class: "mx-auto my-4 border border-black p-4 w-[400px]",
+                onsubmit: move |e| {
+                    async move {
+                        match (e.data.values().get("url")) {
+                            Some(url) => {
+                                tracing::info!("URL: {:?}", url.as_value());
+                                if let Ok(tweet_data) = get_tweet_data(url.as_value()).await {
+                                    tracing::info!("Output: {:?}", tweet_data)
+                                }
+                            },
+                            None => tracing::info!("URL not found.")
+                        }
+                    }
+                },
                 label {
                     "Enter URL to the tweet:"
                 }
@@ -53,10 +66,12 @@ fn Home() -> Element {
                     input {
                         class: "border-b border-black px-1/2 pb-1/2 flex-grow",
                         placeholder: "https://x.com/213123",
-                        "type": "url",
+                        name: "url",
+                        r#type: "url",
                     }
                     button {
                         class: "px-2 py-1 bg-slate-500/25 border border-black",
+                        r#type: "submit",
                         "Search"
                     }               
                 }
@@ -72,6 +87,11 @@ async fn post_server_data(data: String) -> Result<(), ServerFnError> {
 }
 
 #[server(GetServerData)]
-async fn get_server_data() -> Result<String, ServerFnError> {
+async fn get_tweet_data(url: String) -> Result<String, ServerFnError> {
+    tracing::info!("URL: {}", url);
+    // the purpose of this is to fetch a tweet's data from its URL. 
+    // 1. given the URL for a real URL to use oEmbed.
+    // 2. using the new URL fetch the JSON associated to the tweet.
+    // 3. return the tweet data
     Ok("Hello from the server!".to_string())
 }
