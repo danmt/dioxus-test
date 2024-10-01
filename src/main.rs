387178ced0fbcffb535443015f3ca633afd26fb2
Dan <danielarturomt@gmail.com>
2024-10-01 19:25:37 +0100

    feat: use oEmbed to return tweets data as html

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 
 use dioxus::prelude::*;
 use dioxus_logger::tracing;
+use reqwest::Url;
+use serde::{Deserialize, Serialize};
 
 // Urls are relative to your Cargo.toml file
 const _TAILWIND_URL: &str = manganis::mg!(file("public/tailwind.css"));
@@ -36,18 +38,18 @@ fn Home() -> Element {
                 class: "p-4 bg-slate-500/50",
                 h1 { 
                     class: "text-blue-500 text-center text-3xl",
-                    "Verifyah"
-                }
+                    "Verifyah",
+                },
                 p {
                     class: "text-center",
-                    "Avoid scams, use the cryptography for what it's meant to be."
-                }
+                    "Avoid scams, use the cryptography for what it's meant to be.",
+                },
             }
             form {
                 class: "mx-auto my-4 border border-black p-4 w-[400px]",
                 onsubmit: move |e| {
                     async move {
-                        match (e.data.values().get("url")) {
+                        match e.data.values().get("url") {
                             Some(url) => {
                                 tracing::info!("URL: {:?}", url.as_value());
                                 if let Ok(tweet_data) = get_tweet_data(url.as_value()).await {
@@ -60,7 +62,7 @@ fn Home() -> Element {
                 },
                 label {
                     "Enter URL to the tweet:"
-                }
+                },
                 div {
                     class: "flex gap-4",
                     input {
@@ -68,30 +70,43 @@ fn Home() -> Element {
                         placeholder: "https://x.com/213123",
                         name: "url",
                         r#type: "url",
-                    }
+                    },
                     button {
                         class: "px-2 py-1 bg-slate-500/25 border border-black",
                         r#type: "submit",
                         "Search"
-                    }               
-                }
+                    },
+                },
             }
         }
     }
 }
 
-#[server(PostServerData)]
-async fn post_server_data(data: String) -> Result<(), ServerFnError> {
-    tracing::info!("Server received: {}", data);
-    Ok(())
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TweetData {
+    pub html: String,
+    pub author_name: String,
+    pub author_url: String,
+    pub url: String,
 }
 
 #[server(GetServerData)]
-async fn get_tweet_data(url: String) -> Result<String, ServerFnError> {
+async fn get_tweet_data(url: String) -> Result<TweetData, ServerFnError> {
     tracing::info!("URL: {}", url);
-    // the purpose of this is to fetch a tweet's data from its URL. 
-    // 1. given the URL for a real URL to use oEmbed.
-    // 2. using the new URL fetch the JSON associated to the tweet.
-    // 3. return the tweet data
-    Ok("Hello from the server!".to_string())
+
+    // Get the embed URL
+    let embed_url = Url::parse_with_params("https://publish.twitter.com/oembed", &[("url", url)])?;
+    tracing::info!("Embed URL: {}", embed_url);
+
+    // Fetch the tweet's data
+    let response = reqwest::get(embed_url).await?;
+    tracing::info!("Response: {:?}", response);
+
+    // Verify response before parsing
+
+    // Parse data as tweet data
+    let tweet_data = response.json::<TweetData>().await?;
+    tracing::info!("Tweet data: {:?}", tweet_data);
+
+    Ok(tweet_data)
 }
