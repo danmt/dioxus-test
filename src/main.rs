587e2904779e765aecdd4c76ce6416a35dbc6c00
Dan <danielarturomt@gmail.com>
2024-10-01 17:52:27 +0100

    feat: add input for tweet url

--- src/main.rs
+++ src/main.rs
@@ -32,9 +32,8 @@ fn Home() -> Element {
 
     rsx! {
         div {
-            class: "p-8",
             header {
-                class: "border border-black mx-auto p-4 w-[400px]",
+                class: "p-4 bg-slate-500/50",
                 h1 { 
                     class: "text-blue-500 text-center text-3xl",
                     "Verifyah"
@@ -42,6 +41,24 @@ fn Home() -> Element {
                 p {
                     class: "text-center",
                     "Avoid scams, use the cryptography for what it's meant to be."
+                }
+            }
+            div {
+                class: "mx-auto my-4 border border-black p-4 w-[400px]",
+                label {
+                    "Enter URL to the tweet:"
+                }
+                div {
+                    class: "flex gap-4",
+                    input {
+                        class: "border-b border-black px-1/2 pb-1/2 flex-grow",
+                        placeholder: "https://x.com/213123",
+                        "type": "url",
+                    }
+                    button {
+                        class: "px-2 py-1 bg-slate-500/25 border border-black",
+                        "Search"
+                    }               
                 }
             }
         }
